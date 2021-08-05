7b82e2f24b6a3c85b87b70aef48e2ea650844a26
snrmasara <njey60@gmail.com>
2021-08-05 03:24:40 +0300

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    // View index file
+   public function index(){
+       return view('index');
+   }
+
+   //Store Files to database
+   public function store(Request $request){
+    // getting the file
+    $upload=$request->file('upload-file');
+    $filePath=$upload->getRealPath();
+
+    // Opening and Reading the file
+    $file=fopen($filePath, 'r');
+
+    //Reading only the header area
+    $header = fgetcsv($file);
+
+    $escapedHeader=[];
+
+    // Validation
+    foreach ($header as $key => $value) {
+        $lheader=strtolower($value);
+        $escapedItem=preg_replace('/[^a-z]/','', $lheader);
+        array_push($escapedHeader, $escapedItem);
+
+    }
+
+    // Loop through to acquire other columns
+
+        while ($columns = fgetcsv($file)) {
+            if ($columns[0]=="") {
+                continue;
+            }
+
+    // Trimming Data
+            foreach ($columns as $key => &$value) {
+                $value=preg_replace('~/\D~','', $value);
+            }
+        $data = array_combine($escapedHeader,$columns);
+
+        //Datatypes setup
+
+        foreach ($data as $key => $value) {
+            $value=($key=="invoiceno" || $key=="stockcode")?(string) $value:(float)$value;
+
+        }
+        // Update the table
+        $invoiceno=$data['invoiceno'];
+        $stockcode=$data['stockcode'];
+        $description=$data['description'];
+        $quantity=$data['quantity'];
+        $invoicedate=$data['invoicedate'];
+        $unitprice=$data['unitprice'];
+        $customerid=$data['customerid'];
+        $country=$data['country'];
+
+        $post= post::firstOrNew(['invoiceno'=>$invoiceno, 'stockcode'=>$stockcode]);
+        $post->description=$description;
+        $post->quantity=$quantity;
+        $post->invoicedate=$invoicedate;
+        $post->unitprice=$unitprice;
+        $post->customerid=$customerid;
+        $post->country=$country;
+        $post->save();
+
+
+
+        }
+
+
+   }
+}
---
+++ database/migrations/2021_08_04_214027_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('invoiceno');
+            $table->text('description');
+            $table->decimal('quantity');
+            $table->dateTime('invoicedate');
+            $table->float('unitprice');
+            $table->integer('customerid')->nullable();
+            $table->text('country');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
